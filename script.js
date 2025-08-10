31f555d27d8e65c3b6193454056db4d632e5cb14
Arsh001405 <arshkahlon.14.2005@gmail.com>
2025-08-10 13:16:45 -0400

    js for sliders and buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+const bulb = document.querySelector('.light-bulb');
+const powerBtn = document.getElementById('togglePower');
+const brightnessSlider = document.getElementById('brightness');
+const saturationSlider = document.getElementById('saturation');
+
+let isOn = true;
